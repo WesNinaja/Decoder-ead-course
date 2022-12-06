9b1168e2e9d0b19293795cdfcde331c48f81373e
wesley.calle <wesley.calle@idwall.co>
2022-12-06 20:04:09 -0300

    feat: Creating Repositories and Services to impl

---
+++ src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
@@ -0,0 +1,8 @@
+package com.ead.course.services.impl;
+
+import com.ead.course.services.LessonService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LessonServiceImpl implements LessonService {
+}
