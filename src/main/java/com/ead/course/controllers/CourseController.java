9d89ef78cf095b2f8008eb3a511f3c365ad75469
wesley.calle <wesley.calle@idwall.co>
2022-12-07 00:38:53 -0300

    feat: Creating RESTful API for Course with Spring Validation

--- src/main/java/com/ead/course/controllers/CourseController.java
+++ src/main/java/com/ead/course/controllers/CourseController.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/course")
+@RequestMapping("/courses")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class CourseController {
 
