5b3d9b7f8556e747ce03b0315228e46e6935a698
wesley.calle <wesley.calle@idwall.co>
2022-12-07 07:46:46 -0300

    feat: Creating RESTful for Module with Validations

--- src/main/java/com/ead/course/controllers/CourseController.java
+++ src/main/java/com/ead/course/controllers/CourseController.java
@@ -1,6 +1,6 @@
 package com.ead.course.controllers;
 
-import com.ead.course.CourseDto;
+import com.ead.course.dtos.CourseDto;
 import com.ead.course.models.CourseModel;
 import com.ead.course.services.CourseService;
 import org.springframework.beans.BeanUtils;
@@ -25,7 +25,7 @@ public class CourseController {
     CourseService courseService;
 
     @PostMapping
-    public ResponseEntity<Object> sabeCourse(@RequestBody @Valid CourseDto courseDto) {
+    public ResponseEntity<Object> saveCourse(@RequestBody @Valid CourseDto courseDto) {
 
         var courseModel = new CourseModel();
         BeanUtils.copyProperties(courseDto, courseModel);
